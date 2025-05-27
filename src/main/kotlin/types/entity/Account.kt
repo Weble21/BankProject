eca18716fbb1e6159311803cb82bfa534e95604a
Weble21 <89642807+Weble21@users.noreply.github.com>
2025-05-27 22:18:29 +0900

    DTO, Entity 작성

--- src/main/kotlin/types/entity/Account.kt
+++ src/main/kotlin/types/entity/Account.kt
@@ -18,8 +18,8 @@ data class Account (
     @JoinColumn(name = "user_ulid", nullable = false)
     val user: User,
 
-    ,@Column(name = "balance", nullable = false, precision = 15, scale = 2)
-    var balance : BigDecimal = BigDecimal.ZERO
+    @Column(name = "balance", nullable = false, precision = 15, scale = 2)
+    var balance : BigDecimal = BigDecimal.ZERO,
 
     @Column(name = "account_number", length = 100, nullable = false, unique = true)
     val accountNumber : String,
