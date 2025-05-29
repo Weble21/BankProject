aef17a96eb4b7d8913c0954c5b8f8b429d71d44b
Weble21 <89642807+Weble21@users.noreply.github.com>
2025-05-29 22:55:11 +0900

    OAuth2 Interface, Config

---
+++ src/main/kotlin/config/OAuth2Config.kt
@@ -0,0 +1,28 @@
+package org.example.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+/*
+    OAuth2:
+        providers:
+            google:
+                client-id : sample
+                client-secret : <>
+                redirect-uri : <>
+            kakao:
+                client-id : sample
+                client-secret : <>
+                redirect-uri : <>
+ */
+@Configuration
+@ConfigurationProperties(prefix = "oauth2")
+class OAuth2Config {
+    val providers : MutableMap<String, OAuth2ProviderValues> = mutableMapOf()
+}
+
+data class OAuth2ProviderValues (
+    val clientID : String,
+    val clientSecret : String,
+    val redirectUri : String
+)
