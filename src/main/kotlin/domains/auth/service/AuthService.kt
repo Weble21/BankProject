9c2879cc5dd470a0a69c2723a067b4bd1524821a
Weble21 <89642807+Weble21@users.noreply.github.com>
2025-07-07 22:29:21 +0900

    Spring Logging AOP 로깅 극복

--- src/main/kotlin/domains/auth/service/AuthService.kt
+++ src/main/kotlin/domains/auth/service/AuthService.kt
@@ -4,19 +4,24 @@ import jakarta.transaction.Transactional
 import org.example.common.exception.CustomException
 import org.example.common.exception.ErrorCode
 import org.example.common.jwt.JwtProvider
+import org.example.common.logging.Logging
 import org.example.interfaces.OAuthServiceInterface
+import org.slf4j.Logger
 import org.springframework.stereotype.Service
 
 @Service
 class AuthService(
     //앞서 작성된 oAuth key값을 받음
     private val oAuth2Services: Map<String, OAuthServiceInterface>,
-    private val jwtProvider: JwtProvider
+    private val jwtProvider: JwtProvider,
+    private val logger : Logger = Logging.getLogger(AuthService::class.java)
 ) {
     @Transactional
-    fun handleAuth(state: String, code : String) : String {
+    fun handleAuth(state: String, code : String) : String = Logging.logFor(logger) { log ->
         //값을 소문자로 받아야 됨. GOOGLE -> google
         val provider = state.lowercase()
+        log["provider"] = provider
+
         val callService = oAuth2Services[provider] ?: throw CustomException(ErrorCode.PROVIDER_NOT_FOUND, provider)
 
         val accessToken = callService.getToken(code)
