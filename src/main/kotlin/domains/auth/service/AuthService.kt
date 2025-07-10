26b479c6d3bd2a8df29a0759a464c4b8184b5a1c
Weble21 <89642807+Weble21@users.noreply.github.com>
2025-07-10 22:39:14 +0900

    transactional AOP 극복

--- src/main/kotlin/domains/auth/service/AuthService.kt
+++ src/main/kotlin/domains/auth/service/AuthService.kt
@@ -1,10 +1,10 @@
 package org.example.domains.auth.service
 
-import jakarta.transaction.Transactional
 import org.example.common.exception.CustomException
 import org.example.common.exception.ErrorCode
 import org.example.common.jwt.JwtProvider
 import org.example.common.logging.Logging
+import org.example.common.transaction.Transactional
 import org.example.interfaces.OAuthServiceInterface
 import org.slf4j.Logger
 import org.springframework.stereotype.Service
@@ -14,7 +14,8 @@ class AuthService(
     //앞서 작성된 oAuth key값을 받음
     private val oAuth2Services: Map<String, OAuthServiceInterface>,
     private val jwtProvider: JwtProvider,
-    private val logger : Logger = Logging.getLogger(AuthService::class.java)
+    private val logger : Logger = Logging.getLogger(AuthService::class.java),
+    private val transaction : Transactional
 ) {
     @Transactional
     fun handleAuth(state: String, code : String) : String = Logging.logFor(logger) { log ->
@@ -27,6 +28,10 @@ class AuthService(
         val accessToken = callService.getToken(code)
         val userInfo = callService.getUserInfo(accessToken.accessToken)
         val token = jwtProvider.createToken(provider, userInfo.email, userInfo.name, userInfo.id)
+
+/*        transaction.run {
+
+        }*/
 
         //userInfo
     }
